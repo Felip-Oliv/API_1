599f52b4620f5ebd11ec610ace022241430df1ef
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-03-09 20:57:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ const { json } = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const produtos = [];
+
 
 app.get("/",(request, response)=> {
-    return response.status(200).json({massage: "ok"});
+    return response.
+    status(200).
+    json({massage: "Lista de produtos",
+        data: produtos
+
+});
 });
 
 app.post("/", (request, response)=>{
-    return response.status(201).json({message:"ok"});
+    const produto = request.body;
+    produtos.push(produto)
+    return response.
+    status(201).
+    json({
+        message:"Produto cadastrado!",
+        data: produto
+    });
 
 
 });
